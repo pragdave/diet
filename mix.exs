4003fa087f6a708cfe10d55bd7e18e908a421551
pragdave <dave@pragdave.me>
2017-03-02 11:13:21 -0500

    prepare for hex release

--- mix.exs
+++ mix.exs
@@ -1,14 +1,52 @@
 defmodule Diet.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+  @deps    []
+
+  @description """
+  Diet is a DSL for writing your program logic as a sequence of trivial
+  transformations. 
+
+  For example, we could write FizzBuzz as
+
+        defmodule Example.Fizzbuzz do
+
+          use Diet.Transformations
+
+          reductions do
+
+            { :fizzbuzz, num }       -> { :fizzbuzz, num, rem(num, 3), rem(num, 5) }
+
+            { :fizzbuzz, _,   0, 0 } -> "FizzBuzz"
+            { :fizzbuzz, _,   0, _ } -> "Fizz"
+            { :fizzbuzz, _,   _, 0 } -> "Buzz"
+            { :fizzbuzz, num, _, _ } -> to_string(num)
+
+          end
+        end
+
+  These transformations are executed by something called the Stepper.
+
+  See https://github.com/pragdave/diet_examples for some example code, and
+  https://www.youtube.com/watch?v=L1-amhlGk7c for a talk that contains
+  examples of Diet in action.
+  """
+ 
+  # ============================================================
+
+  @in_production Mix.env == :prod
+  
   def project do
     [
       app:     :diet,
-      version: "0.1.0",
+      version: @version,
       elixir:  "~> 1.5-dev",
-      deps:    deps(),
-      build_embedded:  Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
+      deps:    @deps,
+      package: package(),
+      description:     @description,
+      build_embedded:  @in_production,
+      start_permanent: @in_production,
     ]
   end
 
@@ -18,8 +56,20 @@ defmodule Diet.Mixfile do
     ]
   end
 
-  defp deps do
+  defp package do
     [
+      files: [
+        "lib", "mix.exs", "README.md", "LICENSE.md"
+      ],
+      maintainers: [
+        "Dave Thomas <dave@pragdave.me>"
+      ],
+      licenses: [
+        "Apache 2 (see the file LICENSE.md for details)"
+      ],
+      links: %{
+        "GitHub" => "https://github.com/pragdave/diet",
+      }
     ]
-  end
+  end  
 end
