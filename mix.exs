469e889363110a438c15fb283829cee7e7f45534
pragdave <dave@pragdave.me>
2017-03-02 11:16:45 -0500

    prepare for hex release

--- mix.exs
+++ mix.exs
@@ -8,26 +8,6 @@ defmodule Diet.Mixfile do
   Diet is a DSL for writing your program logic as a sequence of trivial
   transformations. 
 
-  For example, we could write FizzBuzz as
-
-        defmodule Example.Fizzbuzz do
-
-          use Diet.Transformations
-
-          reductions do
-
-            { :fizzbuzz, num }       -> { :fizzbuzz, num, rem(num, 3), rem(num, 5) }
-
-            { :fizzbuzz, _,   0, 0 } -> "FizzBuzz"
-            { :fizzbuzz, _,   0, _ } -> "Fizz"
-            { :fizzbuzz, _,   _, 0 } -> "Buzz"
-            { :fizzbuzz, num, _, _ } -> to_string(num)
-
-          end
-        end
-
-  These transformations are executed by something called the Stepper.
-
   See https://github.com/pragdave/diet_examples for some example code, and
   https://www.youtube.com/watch?v=L1-amhlGk7c for a talk that contains
   examples of Diet in action.
